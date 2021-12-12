faa1ddf6f59a9b41d4843385d4160a14f301fe37
brt9029 <brt9029@gmail.com>
2021-12-12 17:26:47 -0600

    more css changes on template page

--- src/page-template.js
+++ src/page-template.js
@@ -1,7 +1,7 @@
 function generateManagerCard(manager) {
     return `
-        <div class="card col-3 m-3 bg-danger">
-            <h5 class="card-title text-center pt-2 text-white bg-primary">${manager.name}</h5>
+        <div class="card col-3 m-3 bg-danger shadow">
+            <h5 class="card-title text-center pt-2 text-white">${manager.name}</h5>
                 <div class="card-body">
                     <h6 class="card-subtitle mb-2 text-white text-center">${manager.getRole()}</h6>
                         <div class="card-body bg-secondary">
@@ -18,8 +18,8 @@ function generateManagerCard(manager) {
 
 function generateEngineerCard(engineer) {
     return `
-    <div class="card col-3 m-3 bg-warning">
-            <h5 class="card-title text-center pt-2 text-white bg-secondary">${engineer.name}</h5>
+    <div class="card col-3 m-3 bg-warning shadow">
+            <h5 class="card-title text-center pt-2 text-white">${engineer.name}</h5>
                 <div class="card-body">
                     <h6 class="card-subtitle mb-2 text-white text-center">${engineer.getRole()}</h6>
                         <div class="card-body bg-secondary">
@@ -36,14 +36,14 @@ function generateEngineerCard(engineer) {
 
 function generateInternCard(intern) {
     return `
-    <div class="card col-3 m-3 bg-success">
-            <h5 class="card-title text-center pt-2">${intern.name}</h5>
+    <div class="card col-3 m-3 bg-success shadow">
+            <h5 class="card-title text-center pt-2 text-white">${intern.name}</h5>
                 <div class="card-body">
                     <h6 class="card-subtitle mb-2 text-white text-center">${intern.getRole()}</h6>
                         <div class="card-body bg-secondary">
                             <ul class="list-group list-group-flush">
                                 <li class="list-group-item">ID: ${intern.id}</li>
-                                <li class="list-group-item">Email: <a href="mailto:${intern.email}>${intern.email}</a></li>
+                                <li class="list-group-item">Email: <a href="mailto:${intern.email}">${intern.email}</a></li>
                                 <li class="list-group-item">School: ${intern.internSchool}</li>
                             </ul>
                         </div>
@@ -71,6 +71,7 @@ module.exports = teamInfo => {
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
         <title>Team Profile</title>
     </head>
     <body>
